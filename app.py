0383eb2051462acdb066b83438a93d26174df3c4
Cristian1109 <149325012+Cristian1109@users.noreply.github.com>
2026-04-08 23:13:04 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,11 +2,13 @@ from flask import Flask, redirect
 
 app = Flask(__name__)
 
-URL = "https://sites.google.com/view/novusticsas/p%C3%A1gina-principal"
-
 @app.route('/')
 def home():
-    return redirect(URL)
+    return redirect("https://sites.google.com/view/novusticsas/p%C3%A1gina-principal")
+
+@app.route('/info')
+def info():
+    return "Servidor Novustic funcionando correctamente"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=10000)
