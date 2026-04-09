dcd1552e3cb88be901c13ed3da94dd1f83ba5083
Cristian1109 <149325012+Cristian1109@users.noreply.github.com>
2026-04-09 14:26:53 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-from flask import Flask, send_file
+from flask import Flask, redirect
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    # Envía un archivo HTML en lugar de redirigir
-    return send_file('index.html')
+    return redirect("https://sites.google.com/view/novusticsas/p%C3%A1gina-principal")
+
+@app.route('/info')
+def info():
+    return "Servidor Novustic funcionando"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=10000)
+    
